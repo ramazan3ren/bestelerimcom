c85093ffbe5dbc172e8dccbb1ec0bed9bcf62c51
ramazan3ren <ramazan3ren@gmail.com>
2024-07-24 23:57:22 +0300

    Fix MIME type for JavaScript files2

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/style.css";
-import { Provider } from "react-redux";
-import store from "@/store/store";
 import { ProviderContextAPI } from "./context/context";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("body")).render(
-  <Provider store={store}>
-    <ProviderContextAPI>
-      <RouterProvider router={routes} />
-    </ProviderContextAPI>
-  </Provider>
+  <ProviderContextAPI>
+    <RouterProvider router={routes} />
+  </ProviderContextAPI>
 );
